c89e4f4442025696162942a15f1b0bdf97a3eb2a
Luke Nelson <lukenels@cs.washington.edu>
2016-03-03 23:41:50 -0800

    Work on mcbot

--- src/mcbot/mcbot.go
+++ src/mcbot/mcbot.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func handleCommand(w http.ResponseWriter, r *http.Request) {
+
+	user := r.FormValue("user_name")
+
+	responseMap := make(map[string]interface{})
+	responseMap["text"] = fmt.Sprintf("Hi %s", user)
+	responseMap["response_type"] = "in_channel"
+	data, err := json.Marshal(responseMap)
+
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(400)
+		return
+	}
+
 	w.WriteHeader(200)
-	w.Write([]byte("This works!\n"))
+	w.Write(data)
+
 }
 
 func main() {
